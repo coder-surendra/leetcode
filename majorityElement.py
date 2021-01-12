cb567b83d46603137e0ecf956e7ba63b2cc7a353
Surendra <coder.surendra@gmail.com>
2021-01-12 12:24:18 +0530

    solved another problem after 2-3 days break

---
+++ majorityElement.py
@@ -0,0 +1,18 @@
+from collections import Counter as counter
+def majorityElement(nums):
+    n = len(nums)
+    # print(n)
+    # x = nums.count(nums)
+    myCounter = counter(nums)
+    # print(myCounter.items())
+    halfN = n // 2
+    # print(halfN)
+    majorElement = 0
+    for key,value in myCounter.items():
+        # print(key ,'->', value)
+        if(value>halfN):
+            majorElement = key
+
+    # print('majorElement = ',majorElement)
+    return majorElement
+majorityElement([2,2,1,1,1,2,2])
